c566e8e3f9596ba7f98f92e38bf40b5bd3ffe364
Hans Trompert <hans.trompert@surf.nl>
2022-11-17 13:59:48 +0100

    use Optional from typing for python3.9 compatibility

--- products/product_blocks/user.py
+++ products/product_blocks/user.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from orchestrator.domain.base import ProductBlockModel
 from orchestrator.types import SubscriptionLifecycle
 
@@ -6,20 +8,20 @@ from products.product_blocks.user_group import UserGroupBlock, UserGroupBlockIna
 
 class UserBlockInactive(ProductBlockModel, lifecycle=[SubscriptionLifecycle.INITIAL], product_block_name="UserBlock"):
     group: UserGroupBlockInactive
-    username: str | None = None
-    age: int | None = None
-    user_id: int | None = None
+    username: Optional[str] = None
+    age: Optional[int] = None
+    user_id: Optional[int] = None
 
 
 class UserBlockProvisioning(UserBlockInactive, lifecycle=[SubscriptionLifecycle.PROVISIONING]):
     group: UserGroupBlockProvisioning
     username: str
-    age: int | None = None
-    user_id: int | None = None
+    age: Optional[int] = None
+    user_id: Optional[int] = None
 
 
 class UserBlock(UserBlockProvisioning, lifecycle=[SubscriptionLifecycle.ACTIVE]):
     group: UserGroupBlock
     username: str
-    age: int | None = None
+    age: Optional[int] = None
     user_id: int
--- products/product_blocks/user_group.py
+++ products/product_blocks/user_group.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from orchestrator.domain.base import ProductBlockModel
 from orchestrator.types import SubscriptionLifecycle
 
@@ -5,13 +7,13 @@ from orchestrator.types import SubscriptionLifecycle
 class UserGroupBlockInactive(
     ProductBlockModel, lifecycle=[SubscriptionLifecycle.INITIAL], product_block_name="UserGroupBlock"
 ):
-    group_name: str | None = None
-    group_id: int | None = None
+    group_name: Optional[str] = None
+    group_id: Optional[int] = None
 
 
 class UserGroupBlockProvisioning(UserGroupBlockInactive, lifecycle=[SubscriptionLifecycle.PROVISIONING]):
     group_name: str
-    group_id: int | None = None
+    group_id: Optional[int] = None
 
 
 class UserGroupBlock(UserGroupBlockProvisioning, lifecycle=[SubscriptionLifecycle.ACTIVE]):
