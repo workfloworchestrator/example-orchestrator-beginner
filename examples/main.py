b65710687a01d789bdf6eb1aaa8dcb49d1578780
Hans Trompert <hans.trompert@surf.nl>
2022-12-02 14:35:20 +0100

    swap application main.py with example main.py

--- examples/main.py
+++ examples/main.py
@@ -2,9 +2,6 @@ from orchestrator import OrchestratorCore
 from orchestrator.cli.main import app as core_cli
 from orchestrator.settings import AppSettings
 
-import products  # noqa: F401  Side-effects
-import workflows  # noqa: F401  Side-effects
-
 app = OrchestratorCore(base_settings=AppSettings())
 
 if __name__ == "__main__":
